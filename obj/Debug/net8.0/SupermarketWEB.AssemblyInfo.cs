1d70317ac2c0983855a28c70f9192e6644e44620
Alejandro <alejandromunozf005@gmail.com>
2024-05-04 00:31:04 -0500

    Create Paymode

--- obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
+++ obj/Debug/net8.0/SupermarketWEB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88cc5c32bd97c6f01068764df58dfe9059f01bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e2fa1cce3a657c646b8bf35366d7ee4c479be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SupermarketWEB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
